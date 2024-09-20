e876e51b8db7a7c93d11b20af56b9fafa173c619
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-09-20 19:50:34 +0530

    Write route to post (create) to-dos

--- server/server.js
+++ server/server.js
@@ -5,6 +5,21 @@ const APP = express();
 const PORT = 5000;
 APP.use(cors());
 APP.use(express.json());
+DB.connect();
+
+APP.post("/todos", async (req, res) => {
+    try {
+        const DESCRIPTION = req.body.description;
+        const NEW_TODO = await DB.query(
+            "INSERT INTO todos (description)\
+            VALUES($1) RETURNING *",
+            [DESCRIPTION]
+        );
+        res.json(NEW_TODO.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 APP.listen(PORT, () => {
     console.log(`Server live on port: ${PORT}`);
