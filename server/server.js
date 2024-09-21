a90671a8a1399eca1f7157dd700170d4a6ef19b6
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-09-21 07:19:32 +0530

    Write route handler to get to-do based on id

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ APP.use(cors());
 APP.use(express.json());
 DB.connect();
 
+// Get all to-dos
 APP.get("/todos", async(req, res) => {
     try {
         const ALL_TODOS = await DB.query(
@@ -17,7 +18,25 @@ APP.get("/todos", async(req, res) => {
         console.log(err.message);
     }
 });
+// Get to-do based on id
+APP.get("/todos/:id", async (req, res) => {
+    try {
+        const REQUESTED_ID = req.params.id;
+        console.log(REQUESTED_ID);
+        const REQUESTED_TODO = await DB.query(
+            "SELECT * FROM todos WHERE todo_id = $1",
+            [REQUESTED_ID]
+        );
+        if (REQUESTED_TODO.rows.length === 0) {
+            return res.status(404).json({ error: "Todo not found" });
+        }
+        res.json(REQUESTED_TODO.rows);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
+// Post a to-do
 APP.post("/todos", async (req, res) => {
     try {
         const DESCRIPTION = req.body.description;
