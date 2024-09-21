3e8b6bc1a67c6c0be3b0eb429c07bd2c44fb1851
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-09-21 13:45:39 +0530

    Build InputTodo component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React, { Fragment } from "react";
 import "./App.css";
+import InputTodo from "./components/InputTodo";
 
 function App() {
-    return <Fragment></Fragment>
+    return (
+        <Fragment>
+            <div className="container">
+                <InputTodo />
+            </div>
+        </Fragment>
+    );
 }
 
 export default App;
